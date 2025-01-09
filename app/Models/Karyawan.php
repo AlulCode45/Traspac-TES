0f10576c9490d85508395f78e8ccc4b5228f265f
AlulCode45 <alulgans70@gmail.com>
2025-01-09 21:16:39 +0700

    feat: adding relationship on model

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Karyawan extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function jabatan(): BelongsTo
+    {
+        return $this->belongsTo(Jabatan::class);
+    }
+
+    public function unit_kerja(): BelongsTo
+    {
+        return $this->belongsTo(UnitKerja::class);
+    }
 }
