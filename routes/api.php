bf8b1053fbe8a025f72a045e176584483b99dbdb
AlulCode45 <alulgans70@gmail.com>
2025-01-09 21:50:48 +0700

    feat: route for karyawan

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\KaryawanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+   Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'handle']);
+   Route::post('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->middleware('auth:sanctum');
+});
+
+Route::prefix('karyawan')->name('karyawan.')->group(function () {
+    Route::apiResource('/', KaryawanController::class)->middleware('auth:sanctum');
+});
