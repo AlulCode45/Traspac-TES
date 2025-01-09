ba4f2c1c3eb0977360b219ca8dd2aeafb084fcb9
AlulCode45 <alulgans70@gmail.com>
2025-01-09 23:57:20 +0700

    feat: add auth login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function handle(Request $request)
+    public function handle(LoginRequest $request)
     {
-
+        try {
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                $user = User::query()->find(Auth::id());
+                $user['token'] = $user->createToken('appToken')->plainTextToken;
+                return ResponseHelper::success(
+                    data: $user->toArray(),
+                    message: 'Login successfully'
+                );
+            }
+        } catch (\Exception $exception) {
+            return ResponseHelper::error(
+                message: $exception->getMessage(),
+            );
+        }
     }
 }
