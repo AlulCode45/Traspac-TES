f280e25b6f7555d752ae8a6e62bf5d282133f05b
AlulCode45 <alulgans70@gmail.com>
2025-01-09 21:18:07 +0700

    feat: crud function on karyawan

--- app/Contracts/Repository/Repository/KaryawanRepository.php
+++ app/Contracts/Repository/Repository/KaryawanRepository.php
@@ -4,8 +4,44 @@ namespace App\Contracts\Repository\Repository;
 
 
 use App\Contracts\Repository\Interfaces\KaryawanInterface;
+use App\Models\Karyawan;
 
 class KaryawanRepository extends BaseRepository implements KaryawanInterface
 {
+    public function __construct(Karyawan $model)
+    {
+        $this->model = $model;
+    }
 
+    public function get(): mixed
+    {
+        return $this->model->query()
+            ->with(['jabatan', 'unit_kerja'])
+            ->get();
+    }
+
+    public function show(mixed $id): mixed
+    {
+        return $this->model->query()
+            ->with(['jabatan', 'unit_kerja'])
+            ->findOrFail($id);
+    }
+
+    public function insert(array $data): mixed
+    {
+        return $this->model->query()
+            ->insert($data);
+    }
+
+    public function update(mixed $id, array $data): mixed
+    {
+        return $this->show($id)
+            ->update($data);
+    }
+
+    public function delete(mixed $id): bool
+    {
+        return $this->show($id)
+            ->delete();
+    }
 }
