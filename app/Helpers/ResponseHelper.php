4d699b2f7171c0a6b43a631c456db03de7986f96
AlulCode45 <alulgans70@gmail.com>
2025-01-12 20:58:42 +0700

    feat: add docblock

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -2,13 +2,22 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
 class ResponseHelper
 {
+    /**
+     * @param mixed $data
+     * @param string $message
+     * @param int $status
+     * @return JsonResponse
+     */
     public static function success(
         mixed $data = [],
         string $message = 'Retrieved successfully.',
-        int $status = 200,
-    )
+        int $status = Response::HTTP_OK,
+    ): JsonResponse
     {
         return response()->json([
             'meta' => [
@@ -19,11 +28,18 @@ class ResponseHelper
         ],$status);
     }
 
+    /**
+     * @param mixed $error
+     * @param string $message
+     * @param int $status
+     * @return JsonResponse
+     */
     public static function error(
         mixed $error = [],
         string $message = 'Something went wrong.',
-        int $status = 500,
-    ){
+        int $status = Response::HTTP_BAD_REQUEST,
+    ): JsonResponse
+    {
         return response()->json([
             'meta' => [
                 'message' => $message,
