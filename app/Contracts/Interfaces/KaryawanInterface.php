7d9b86ed2e80d2a4d1e93e9b9da71dfed36e8abb
AlulCode45 <alulgans70@gmail.com>
2025-01-13 19:42:16 +0700

    feat: adding count interface

--- app/Contracts/Interfaces/KaryawanInterface.php
+++ app/Contracts/Interfaces/KaryawanInterface.php
@@ -2,13 +2,14 @@
 
 namespace App\Contracts\Interfaces;
 
+use App\Contracts\Interfaces\Eloquent\CountInterface;
 use App\Contracts\Interfaces\Eloquent\DeleteInterface;
 use App\Contracts\Interfaces\Eloquent\GetInterface;
 use App\Contracts\Interfaces\Eloquent\ShowInterface;
 use App\Contracts\Interfaces\Eloquent\StoreInterface;
 use App\Contracts\Interfaces\Eloquent\UpdateInterface;
 
-interface KaryawanInterface extends GetInterface, ShowInterface, DeleteInterface,UpdateInterface,StoreInterface
+interface KaryawanInterface extends GetInterface, ShowInterface, DeleteInterface,UpdateInterface,StoreInterface,CountInterface
 {
 
 }
