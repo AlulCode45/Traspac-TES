8bfae45ac84d5935bdd52a41c97f834754011b26
AlulCode45 <alulgans70@gmail.com>
2025-01-14 08:34:08 +0700

    feat: adding service to handle upload file

--- app/Services/KaryawanService.php
+++ app/Services/KaryawanService.php
@@ -2,7 +2,16 @@
 
 namespace App\Services;
 
+use App\Traits\UploadTrait;
+use Illuminate\Http\File;
+
 class KaryawanService
 {
+    use UploadTrait;
+    public function validateAndUpload(string $disk, object $file, string $old_file = null): string
+    {
+        if ($old_file) $this->remove($old_file);
 
+        return $this->upload($disk, $file);
+    }
 }
