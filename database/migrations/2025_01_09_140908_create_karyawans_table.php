2986827b3f4df15fe821c02309b873cd585bd4e4
AlulCode45 <alulgans70@gmail.com>
2025-01-14 08:33:49 +0700

    feat: adding photo_profile column on pegawai

--- database/migrations/2025_01_09_140908_create_karyawans_table.php
+++ database/migrations/2025_01_09_140908_create_karyawans_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('karyawans', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('nip')->unique();
+            $table->string('photo_profile')->default('default.jpg');
             $table->string('nama');
             $table->string('tempat_lahir')->nullable();
             $table->date('tanggal_lahir');
